44482c337595a44fde8e63d39c886b261adee259
Malekith <jingenito@vcssoftware.com>
2020-02-12 09:58:59 -0500

    changed the name of question1 to Question1

--- Project1/Question1/question1.m
+++ Project1/Question1/question1.m
@@ -1,6 +1,6 @@
 %The implementation of functions f and g, runs a loop to calculate the
 %desired values.
-function question1()
+function Question1()
 
 %prints the functions f and g for the 
 %x value in the loop
