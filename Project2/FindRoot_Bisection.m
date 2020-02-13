56458e75554c2a0d8582b2e44c5de85f543df00c
jingenito <jingenito1530@gmail.com>
2020-02-12 19:15:16 -0500

    got project2 working

--- Project2/FindRoot_Bisection.m
+++ Project2/FindRoot_Bisection.m
@@ -1,14 +1,16 @@
-function root = FindRoot_Bisection(a, b)
+function root = FindRoot_Bisection(a, b, tol)
 
 if f(a)*f(b) < 0 
     c = (a + b) / 2;
     
-    while f(c) ~= 0
+    while f(c) >= tol
         if f(a)*f(c) < 0
             b = c;
         else 
             a = c;
         end
+        
+        c = (a + b) / 2;
     end
     
     root = c;
